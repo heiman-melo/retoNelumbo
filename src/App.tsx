429f04eef96bcb3eeb347d630d55716a94b51147
heiman <heiman.castro@alivi.com>
2024-02-18 19:03:36 -0500

    terminado el modal de la pagina 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { AppRouterProvider } from '../src/routes/Routes'
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import Header from './components/header'
+import { AppRouterProvider } from "../src/routes/Routes";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header";
+import { Footer } from "antd/es/layout/layout";
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <BrowserRouter basename="/">
-      <AppRouterProvider />
-    </BrowserRouter>
+      <Header />
+      <div className="main">
+        <BrowserRouter basename="/">
+          <AppRouterProvider />
+        </BrowserRouter>
+      </div>
+      <Footer className="footer">Portal de Garantías MACROPAY</Footer>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,13 +1,76 @@
-import { Button, Card, Steps, Typography } from "antd";
+import { Button, Card, Col, Modal, Row, Steps, Typography, Image } from "antd";
 import "../css/color.css";
 import "../css/detail.css";
 import { useNavigate } from "react-router-dom";
 import { CloseOutlined, SmileOutlined } from "@ant-design/icons";
+import { useState, useEffect } from "react";
+import TicketWaves from "../assets/ticket-waves.svg";
 function Detail() {
   const navigate = useNavigate();
+
+  const [visible, setVisible] = useState(false);
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    setVisible(true);
+  }, []);
   return (
     <>
       <div className="containerDetail">
+        <Modal
+          className="modalDetail"
+          open={visible}
+          footer={null}
+          width={650}
+          onCancel={handleClose}
+          centered={true}
+        >
+          <Image preview={false} alt="" src={TicketWaves} />
+          <div className="titleModal">
+            <Typography.Title level={3}>Detalles del Folio</Typography.Title>
+            <Typography.Text>
+              {" "}
+              Algunos detalles del Folio Consultado!
+            </Typography.Text>
+          </div>
+          <div className="rowContainer">
+            <Row gutter={[0,32]}>
+              <Col span={8}>
+                <Typography.Text>Folio de Servicio</Typography.Text>
+                <Typography.Title level={5}>20171828188828</Typography.Title>
+              </Col>
+              <Col span={8}>
+                <Typography.Text>Fecha De Levantamiento</Typography.Text>
+                <Typography.Title level={5}>20/10/2023</Typography.Title>
+              </Col>
+              <Col span={8}>
+                <Typography.Text>Fecha De Finalización</Typography.Text>
+                <Typography.Title level={5}>20/10/2023</Typography.Title>
+              </Col>
+              <Col span={8}>
+                <Typography.Text>IMEI</Typography.Text>
+                <Typography.Title level={5}>354647089752389</Typography.Title>
+              </Col>
+              <Col span={8}>
+                <Typography.Text>Equipo</Typography.Text>
+                <Typography.Title level={5}>
+                  Samsung Galaxy A12
+                </Typography.Title>
+              </Col>
+              <Col span={8}>
+                <Typography.Text>Sucursal De Levantamiento</Typography.Text>
+                <Typography.Title level={5}>Centro Av. 487</Typography.Title>
+              </Col>
+              <Col span={24}>
+                <Typography.Text>Detalles de Averías y Observaciones</Typography.Text>
+                <Typography.Title level={5} style={{lineHeight:"22px",marginTop:"2px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography.Title>
+              </Col>
+            </Row>
+          </div>
+        </Modal>
         <Card className="cardDetail">
           <div className="headerDetail">
             <div className="containerButtonTitle">
@@ -16,7 +79,9 @@ function Detail() {
                 shape="circle"
                 icon={<CloseOutlined />}
                 className="buttonCloseDetail"
-                onClick={() => {navigate(`/`)}}
+                onClick={() => {
+                  navigate(`/`);
+                }}
               />
               <div className="title">
                 <Typography.Title level={3}>
@@ -28,7 +93,10 @@ function Detail() {
               </div>
             </div>
             <div className="stepDetailContainer">
-              <Steps current={0} items={[{}, {}, {},{icon:<SmileOutlined/>}]} />
+              <Steps
+                current={0}
+                items={[{}, {}, {}, { icon: <SmileOutlined /> }]}
+              />
             </div>
           </div>
         </Card>
